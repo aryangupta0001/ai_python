d514dd8c87edf7ab531fe7b4d1ce04e1742ab7db
Aryan Gupta <aryanguptabareilly067@gmail.com>
2024-09-27 12:18:36 +0530

    Leap Year

---
+++ leapyear.py
@@ -0,0 +1,20 @@
+def checkLeap(a):
+    if(a%4 == 0):
+        if(a%100 == 0):
+            if(a%400 == 0):
+                return "Leap Year"
+            else:
+                return "Not a Leap Year"
+            
+        else:
+            if(a%400 != 0):
+                return "Leap Year"
+    
+    else:
+        return "Not a Leap Year"
+
+        
+
+a = int(input("Enter the year : "))
+
+print(checkLeap(a))
