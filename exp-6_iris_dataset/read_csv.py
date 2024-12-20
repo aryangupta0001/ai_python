e1b6453ce53e4f47c89d4616b2aa67ea7698ccd2
Aryan Gupta <aryanguptabareilly067@gmail.com>
2024-12-21 01:02:40 +0530

    iris dataset visualisation

---
+++ exp-6_iris_dataset/read_csv.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("./exp-6_iris_dataset/iris_data.csv")
+print(df.head())
+x_axis = 'sepal_length'
+y_axis = 'sepal_width'
+title = "Sepal Length v/s Width"
+
+# plt.plot(df[x_axis], df[y_axis], marker = 'o', linestyle = '-', color = 'b')
+# plt.title("Iris Data Projection")
+# plt.xlabel(x_axis)
+# plt.ylabel(y_axis)
+
+plt.figure()
+
+ax = plt.axes()
+ax.scatter(df[x_axis], df[y_axis])
+ax.set_xlabel(x_axis)
+ax.set_ylabel(y_axis)
+ax.set_title(title)
+
+plt.show()
